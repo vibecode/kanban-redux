bcb32f6b6f6182fa753aee677620f8d47876cd68
vibecode <rumashkin@gmail.com>
2017-06-06 15:28:55 +0200

    create note actions

---
+++ src/actions/notes.js
@@ -0,0 +1,29 @@
+import uuid from 'uuid';
+
+export const CREATE_NOTE = 'CREATE_NOTE';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
+
+export const createNote = (note) => {
+  return {
+    type: CREATE_NOTE,
+    note: {
+      id: uuid,
+        ...note
+    }
+  }
+};
+
+export const updateNote = (note) => {
+  return {
+    type: UPDATE_NOTE,
+    ...note
+  }
+};
+
+export const deleteNote = (id) => {
+ return {
+   type: DELETE_NOTE,
+   id
+ }
+};
