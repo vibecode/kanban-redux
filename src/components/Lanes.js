af6d4f003ecdd7605f637b24d82e047f011cfc89
vibecode <rumashkin@gmail.com>
2017-06-09 14:50:23 +0200

    add Lanes component

---
+++ src/components/Lanes.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Lane from '../containers/Lane';
+import PropTypes from 'prop-types';
+
+class Lanes extends Component {
+  static propTypes = {
+    lanes: PropTypes.array.isRequired,
+    onEditLane: PropTypes.func.isRequired,
+    onDeleteLane: PropTypes.func.isRequired,
+    onMoveLane: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { onEditLane, onDeleteLane, onMoveLane } = this.props;
+
+    const lanes = this.props.lanes.map(lane => (
+       <Lane
+           key={lane.id}
+           lane={lane}
+           onEditLane={onEditLane}
+           onDeleteLane={onDeleteLane}
+           onMoveLane={onMoveLane} />
+    ));
+
+    return (
+        <div className="lanes">
+          {lanes}
+        </div>
+    )
+  }
+}
+
+export default Lanes;
