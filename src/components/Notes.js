2312ce32d56b720fb67219bba9cbad03c233d896
vibecode <rumashkin@gmail.com>
2017-06-12 23:02:29 +0200

    add prop types

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import Note from './Note';
 import Editable from './Editable';
+import PropTypes from 'prop-types';
 
 class Notes extends Component {
+  static propTypes = {
+    notes: PropTypes.array.isRequired,
+    onDeleteNote: PropTypes.func.isRequired,
+    onEditNote: PropTypes.func.isRequired,
+    onMoveNote: PropTypes.func.isRequired,
+    onValueClick: PropTypes.func.isRequired,
+  };
 
   render() {
     const { notes, onValueClick, onEdit, onDelete } = this.props;
